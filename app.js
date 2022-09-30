cc19a10bf226b840f29087d2eb4d3c9ee985b30b
Hardik <hardik182002@gmail.com>
2022-09-30 17:25:55 +0530

    Skeleton made with Static Input

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const https = require("https");
 const app = express();
-const city="Jabalpur";
+const city="London";
 const apiId="d3d7e7b62558f3717345c1b7cd1f8ee0";
+
 
 const url =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}&units=metric`;
@@ -14,10 +15,19 @@ app.get("/", (req, res) => {
         response.on("data",(data)=>{
             const weatherData=JSON.parse(data);
             console.log(weatherData);
+            const temp=weatherData.main.temp;
+            const description= weatherData.weather[0].description;
+            const icon=weatherData.weather[0].icon;
+            const img=`http://openweathermap.org/img/wn/${icon}@2x.png`;
+            res.write(`<h1>The Current Temperature of ${city} is ${temp}</h1>
+                        <h3>The current Weather is ${description}</h3>`);
+            
+            res.write(`<img src="${img}" alt="${description}" >`);
+            res.send();
         });
     });
 
-    res.send("Hiii");
+    
 });
 
 app.listen(3000, () => {
