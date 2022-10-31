4673019902d5db27d6a7f6da8561de027d3c7a45
Hardik <hardik182002@gmail.com>
2022-10-31 18:51:35 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const https = require("https");
 const bodyParser=require("body-parser");
+
 const app = express();
+app.set('view engine','ejs');
 // const city="London";
 const apiId="d3d7e7b62558f3717345c1b7cd1f8ee0";
 
@@ -15,7 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/",(req,res)=>{
-
+    
     console.log("Post request received.");
     const city=req.body.cityName;
     const url =
@@ -31,11 +33,13 @@ app.post("/",(req,res)=>{
         const description= weatherData.weather[0].description;
         const icon=weatherData.weather[0].icon;
         const img=`http://openweathermap.org/img/wn/${icon}@2x.png`;
-        res.write(`<h1>The Current Temperature of ${city} is ${temp}</h1>
-                    <h3>The current Weather is ${description}</h3>`);
+        // res.write(`<h1>The Current Temperature of ${city} is ${temp} degree Celcius</h1>
+        //             <h3>The current Weather is ${description}</h3>`);
         
-        res.write(`<img src="${img}" alt="${description}" >`);
-        res.send();
+        // res.write(`<img src="${img}" alt="${description}" >`);
+        // res.send();
+
+        res.render('index',{city: city, temp:temp, description:description,img:img})
     });
 });
 
